be5786b89f0b9c80bbdb8e467c095e20c16540f6
Carrington <Manyuchi>
2023-10-21 09:24:25 +0200

    MVVM Structure

--- RickAndMortyApp/Controllers/Core/RMTabController.swift
+++ RickAndMortyApp/Controllers/Core/RMTabController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RMTabController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ RickAndMortyApp/Models/RMEpisode.swift
@@ -0,0 +1,8 @@
+//
+//  RMEpisode.swift
+//  RickAndMortyApp
+//
+//  Created by DA MAC M1 157 on 2023/10/21.
+//
+
+import Foundation
